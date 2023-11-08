626a8487243b868c612c86dc24dfb1704419720c
Phil Broadley <philbrocoding@hotmail.com>
2023-11-08 21:24:27 +0000

    login

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,5 +1,36 @@
 import axios from "axios";
+import { useState } from "react";
+import { Button } from "@mui/material";
 
 export default function Login() {
-  return <main className="login">LOGIN</main>;
+    const url = "http://localhost:9000";
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = () => {
+        let data = {
+            email: email,
+            password: password
+        };
+
+        axios.post(`${url}/customers/login`, data).then(res => {
+            console.log(res).catch(err => {
+                console.log(err);
+            });
+        });
+    };
+
+    return (
+        <main className="login">
+            <h2>Log in to your account</h2>
+            <label>Email</label>
+            <input type="email" onChange={e => setEmail(e.target.value)} />
+            <label>Password</label>
+            <input
+                type="password"
+                onChange={e => setPassword(e.target.value)}
+            />
+            <Button onClick={handleLogin}>Login</Button>
+        </main>
+    );
 }
