702f33074a19c7766713d32788ce272bcef8cc13
Phil Broadley <philbro1992@hotmail.co.uk>
2023-12-10 13:58:59 +0000

    work on account page

---
+++ src/tests/login.test.js
@@ -0,0 +1,6 @@
+import { render, fireEvent } from "@testing-library/react";
+import Login from "../routes/Login";
+
+describe("User login", () => {
+    test("User can log in if all details are valid", () => {});
+});
